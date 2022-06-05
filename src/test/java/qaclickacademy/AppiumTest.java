d4ba20335c586e8310ccfcdd2339b4a4e91ed81e
Neha <nehalahoti28@gmail.com>
2022-06-05 23:17:15 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+    @Test
+    public void AppiumTest1(){
+        System.out.println("Appium1");
+    }
+
+    @Test
+    public void AppiumTest2(){
+        System.out.println("Appium2");
+    }
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,15 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+    @Test
+    public void SeleniumTest1(){
+        System.out.println("Selenium1");
+    }
+
+    @Test
+    public void SeleniumTest2(){
+        System.out.println("Selenium2");
+    }
+}
